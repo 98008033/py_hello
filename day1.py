3be5356f62bd6f572d7e68690e38d8cc47dca17d
hyc <enterpwy@gmail.com>
2016-03-15 14:41:07 +0800

    first commit from pyCharm

--- day1.py
+++ day1.py
@@ -1,1 +1,17 @@
-print("hello world")
+# math库
+# 160315
+import math
+
+# math.ceil（x） 向上取整
+a=math.ceil(-0.1)
+print(a)
+a=math.ceil(0.1)
+print(a)
+
+
+# math.copysign(x,y)
+
+a=math.copysign(1,1)
+print(a)
+a=math.copysign(-10,2)
+print(a)
